5abe85ca25115b6b5ab4455e4979f4e1f19f1f09
Brandon Liu <liuy01510.work@gmail.com>
2023-07-12 14:52:11 +0000

    Added proto files output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub mod Scraping {
+    pub mod Requests {
+        include!(
+            concat!(
+                env!(
+                    "OUT_DIR"
+                ),
+                "/scraping.requests.rs"
+            )
+        );
+    }
+    pub mod Results {
+        include!(
+            concat!(
+                env!(
+                    "OUT_DIR"
+                ),
+                "/scraping.results.rs"
+            )
+        );
+    }
+}
+
+
+pub mod Sources {
+
+}
