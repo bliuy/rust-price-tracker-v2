122264adfec8c3461f23468ad8ed823573ca6e9f
Brandon Liu <liuy01510.work@gmail.com>
2023-07-14 15:23:53 +0000

    Ran linter.

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() {
     prost_build::compile_protos(
         &["protobuf/requests.proto", "protobuf/results.proto"],
-        &["protobuf"]
-    ).expect("Failed to build protobuf.")
+        &["protobuf"],
+    )
+    .expect("Failed to build protobuf.")
 }
