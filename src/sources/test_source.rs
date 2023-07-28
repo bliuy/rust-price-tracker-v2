2f4e2bb0307636e5581ebdbe8ff4ee6b15d724f4
Brandon Liu <liuy01510.work@gmail.com>
2023-07-28 00:30:17 +0000

    Implemented Test Source.

--- src/sources/test_source.rs
+++ src/sources/test_source.rs
@@ -1,7 +1,31 @@
-use crate::{scraping, scraping_traits};
+use std::error::Error;
+
+use async_trait::async_trait;
+use reqwest::Client;
+
+use crate::{
+    scraping::{self, requests::Test},
+    scraping_traits::{self, BaseTraits, Scraper},
+};
 
 impl scraping_traits::Source for scraping::requests::Test {
     fn get_source_name(&self) -> String {
         "test".to_string()
+    }
+}
+
+impl BaseTraits for Test {}
+
+#[async_trait]
+impl scraping_traits::Scraper for Test {
+    fn get_unique_id(&self) -> String {
+        format!("Test-payload")
+    }
+
+    async fn scrape(
+        &self,
+        client: &Client,
+    ) -> Result<scraping::results::ScrapingResult, Box<dyn Error + Send>> {
+        todo!()
     }
 }
