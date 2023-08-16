984c069f6366c628025421361815e69fc9a040dd
Brandon Liu <liuy01510.work@gmail.com>
2023-08-16 14:39:28 +0000

    Adding ScrapingResultJson struct.

--- src/scraping.rs
+++ src/scraping.rs
@@ -4,3 +4,34 @@ pub mod requests {
 pub mod results {
     include!(concat!(env!("OUT_DIR"), "/scraping.results.rs"));
 }
+
+pub mod json_results {
+    use std::collections::HashMap;
+
+    use super::results::ScrapingResult;
+
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct ScrapingResultJson {
+        source: String,
+        utc_timestamp: u64,
+        name: String,
+        identifier: String,
+        price: f32,
+        attributes: HashMap<String, String>,
+        metadata: HashMap<String, String>,
+    }
+
+    impl From<ScrapingResult> for ScrapingResultJson {
+        fn from(value: ScrapingResult) -> Self {
+            ScrapingResultJson {
+                source: value.source,
+                utc_timestamp: value.utc_timestamp,
+                name: value.name,
+                identifier: value.identifier,
+                price: value.price,
+                attributes: value.attributes,
+                metadata: value.metadata,
+            }
+        }
+    }
+}
